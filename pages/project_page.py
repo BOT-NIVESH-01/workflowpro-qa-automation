847ded38432a7cdc453765f3fccdfea058220f5c
BOT-NIVESH-01 <23bq1a4745@vvit.net>
2026-07-14 12:42:25 +0530

    Create Project Page Object

--- pages/project_page.py
+++ pages/project_page.py
@@ -1,7 +1,18 @@
-"""Project page object placeholder."""
+from pages.base_page import BasePage
 
 
-class ProjectPage:
-    """Placeholder project page object."""
+class ProjectPage(BasePage):
 
-    pass
+    PROJECT_NAME = "#project-name"
+    CREATE_BUTTON = "#create-project"
+    DELETE_BUTTON = "#delete-project"
+
+    def __init__(self, page):
+        super().__init__(page)
+
+    def create_project(self, name):
+        self.fill(self.PROJECT_NAME, name)
+        self.click(self.CREATE_BUTTON)
+
+    def delete_project(self):
+        self.click(self.DELETE_BUTTON)
